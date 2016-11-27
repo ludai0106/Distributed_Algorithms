0182036a566df816ad50387f4d65a997c9345598
ludai0106 <ludai0106@gmail.com>
2016-11-27 19:27:11 +0100

    upload interface names

---
+++ Lab_1/src/clock.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ludai on 16/11/27.
+ */
+public class clock {
+    private int[] vector;
+    public clock(int size){
+        vector = new int[size];
+    }
+}
---
+++ Lab_1/src/message.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by ludai on 16/11/27.
+ */
+public class message {
+}
