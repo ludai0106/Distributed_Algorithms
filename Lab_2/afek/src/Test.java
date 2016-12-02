5f5c47fa4b3c88e5ec2edaf0a127422ccfbbef2d
Kevinkyea <kevinkeya.lan@gmail.com>
2016-12-02 10:32:22 +0100

    Support console

--- Lab_2/afek/src/Test.java
+++ Lab_2/afek/src/Test.java
@@ -6,18 +6,29 @@ import java.rmi.registry.LocateRegistry;
 
 public class Test {
 
-	static final int networkSize = 200;
+	static final int networkSize = 2;
 	static int rPort = 1099;
 
 	public static void main(String args[]) throws RemoteException, AlreadyBoundException, NotBoundException {
-		LocateRegistry.createRegistry(rPort);
-		for (int i = 1; i <= networkSize; i++) {
-			int processId = 1000 + i;
+		if (args == null || args.length == 0) {
+			LocateRegistry.createRegistry(rPort);
+			for (int i = 1; i <= networkSize; i++) {
+				int processId = 1000 + i;
+				Node node;
+				if (i % 2 != 0)
+					node = new Node(networkSize, rPort, processId, 1);
+				else
+					node = new Node(networkSize, rPort, processId, 0);
+				node.notifyOthers();
+				System.out.println(node.getOriginProcessId() + ":\tWaiting for the incoming messages...");
+			}
+		} else {
+			LocateRegistry.createRegistry(rPort);
 			Node node;
-			if (i % 2 != 0)
-				node = new Node(networkSize, rPort, processId, 1);
-			else
-				node = new Node(networkSize, rPort, processId, 0);
+			int Id = Integer.parseInt(args[0]);
+			int status = Integer.parseInt(args[1]);
+			int processId = 1000 + Id;
+			node = new Node(networkSize, rPort, processId, 1);
 			node.notifyOthers();
 			System.out.println(node.getOriginProcessId() + ":\tWaiting for the incoming messages...");
 		}
